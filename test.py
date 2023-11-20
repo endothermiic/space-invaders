801b0c97afadaebf0b948ad2c1b20e40c020bfb5
endothermiic <103846051+endothermiic@users.noreply.github.com>
2023-11-20 04:17:40 +0000

    new file:   si.mif 	new file:   test.py

---
+++ test.py
@@ -0,0 +1,44 @@
+import sys
+
+from PIL import Image
+
+image_name = "si.png"
+def main(image_name):
+    image = Image.open(image_name, 'r')
+    pixels = list(image.getdata())
+
+    mif_name = image_name.split('.')[0] + '.mif'
+
+    mif_file = open(mif_name, 'w+')
+
+    mif_file.write(
+        'DEPTH={};\nWIDTH={};\nADDRESS_RADIX=UNS;\nDATA_RADIX=BIN;\nCONTENT\nBEGIN\n\n'.format(len(pixels), 3))
+    address = 0
+    for i in range(image.size[1]):
+        for j in range(image.size[0]):
+            mif_file.write("[" + str(address) + "]" + ": ")
+            mif_file.write(' ' + three_bit_conversion(pixels[address]))
+            address += 1
+            mif_file.write(';\n')
+
+    mif_file.write('END;\n')
+
+    mif_file.close()
+
+    image.close()
+
+
+def three_bit_conversion(rgb):
+    bits = []
+    for i in range(len(rgb)):
+        if rgb[i] > 128:
+            bits.append(1)
+        else:
+            bits.append(0)
+
+    return "{}{}{}".format(bits[0], bits[1], bits[2])
+
+
+if __name__ == '__main__':
+    for arg in sys.argv[1:]:
+        main(arg)
