29b06af6277aa1c14ac8fab0e0ccbb43eccab8b3
sovasofiia <137102108+sovasofiia@users.noreply.github.com>
2023-11-28 15:36:49 -0500

    Update shotsControl.v

--- shotsControl/shotsControl.v
+++ shotsControl/shotsControl.v
@@ -1,15 +1,17 @@
 module shotsControl (clk, keyPressed, topReached, collidedWith Alien, updatedRocketPosition, userIntakeEn, updatePositionEn, waitingEn );
 
 	input clk, keyPressed, topReached, collidedWithAlien, updatedRocketPosition;
-	output reg userIntakeEn, updatePositionEn, waitingEn;
+	output reg userIntakeEn, updatePositionEn, waitingEn, drawEn;
 	
 	reg [2:0] current_state, next_state;
 	
 	localparam INTAKE = 2'd0, // -> set frame counter for top boundary check to 0
-										//if rocket exists - erase it (ex if we reached top of screen or killed an alien)
+				  //if rocket exists - erase it (ex if we reached top of screen or killed an alien)
 												
 					
-				  UPDATE_POSITION = 2'd1, //update rocket position, draw first rocket position, pass the coordinates back to check for collision
+				  UPDATE_POSITION = 2'd1, //update rocket position, 
+							  //draw rocket position, 
+						          //pass the coordinates to the alienTalentManager back to check for collision
 				  
 				  WAIT = 2'd2; //check for collisiton, check for top reached, 
 	
@@ -35,10 +37,11 @@ always @(*)
 		userIntakeEn <= 1'b0;
 		updatePositionEn <= 1'b0;
 		waitingEn <= 1'b0;
+	   	drawEn <= 1'b0;
 		
 	case(current_state)
 		INTAKE: userIntakeEn <= 1'b1;
-		UPDATE_POSITION: updatePositionEn <= 1'b1;
+		UPDATE_POSITION: updatePositionEn <= 1'b1, drawEn <= 1'b1;
 		WAIT: waitingEn <= 1'b1;
 	endcase
 		
