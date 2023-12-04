11c95d9b32c573b756efd27f152c824091f2a60d
endothermiic <103846051+endothermiic@users.noreply.github.com>
2023-12-04 07:10:50 -0500

    Update aliensMove.v

--- alienControl/aliensMove.v
+++ alienControl/aliensMove.v
@@ -1,14 +1,23 @@
-module aliensMove(input clk, reset, input [7:0]xtop, input [6:0]ytop, input [7:0]xbottom, input [6:0]ybottom, input clear1, clear2, clear3, clear4, clear5, moveDown, 
+`include "gameovernew.v"
+`include "alienrow.v"
+`include "youWin.v"
+
+
+
+
+module aliensDatapath(input clk, reset, input [7:0]xtop, input [6:0]ytop, input [7:0]xbottom, input [6:0]ybottom, input clear1, clear2, clear3, clear4, clear5, moveDown, 
 				output reg [7:0]xout, 
 				output reg [6:0]yout, 
 				output reg [2:0]colourOut,
 				output reg aliensMoveEn,
-				output reg cleared1, cleared2, cleared3, cleared4, cleared5, movedDown);
+				output reg cleared1, cleared2, cleared3, cleared4, cleared5, movedDown, clearAny);
 
 parameter X_SCREEN_PIXELS = 8'd160;
 parameter Y_SCREEN_PIXELS = 7'd120;
 parameter xStart = 8'd10; //start of 11x10 square
 parameter yStart = 7'd10;
+parameter ALIEN1_X = 8'd10;
+parameter ALIEN1_Y = 7'd10;
 
 reg [7:0] xorig = xStart; //should start at here
 reg [6:0] yorig = yStart;
@@ -17,7 +26,6 @@ reg yIncremented = 1'b0;
 
 reg [10:0] address = 11'b0;
 wire [2:0] aliensColour, gameOverColour, youWinColour;
-assign wire clearAny = clear1 || clear2 || clear3 || clear4 || clear5;
 reg startRow, clear1set, clear2set, clear3set, clear4set, clear5set;
 reg gameWon, gameDone;
 
@@ -25,23 +33,27 @@ reg [14:0]addressFS = 15'b0;
 
 //check alienRow instantiation
 
-alienRow a0 (.address(address), .data(3'd000), .writeEn(clearAny), .qr(aliensColour));
-gameOver g0 (.clock(clk), .address(addressFS), .q(gameOverColour)); //has game over mif
-youWin w0 (.clock(clk), .address(addressFS), .q(youWinColour)); //has you win mif
+ar a0 (.clock(clk), .address(address), .data(3'd000), .wren(clearAny), .q(aliensColour));
+g g0 (.clock(clk), .address(addressFS), .q(gameOverColour)); //has game over mif
+yw w0 (.clock(clk), .address(addressFS), .q(youWinColour)); //has you win mif
 
 
 //at moveDown, decrement y and cycle through all xy in the block
 always@(posedge clk)
 	begin
-		if (reset) //if one of the clears is high, replace PERTINENT memory addresses with 000
+		if (~reset) //if one of the clears is high, replace PERTINENT memory addresses with 000
 			begin
 				xout <= 7'b0;
-				yout <= 6'b0;
+				yout <= yStart;
 				cleared1 <= 1'b0; 
 				cleared2 <= 1'b0; 
 				cleared3 <= 1'b0;
 				cleared4 <= 1'b0;
 				cleared5 <= 1'b0;
+				gameWon <= 1'b0;
+				gameDone <= 1'b0;
+				clearAny <= 1'b0;
+				startRow <= 1'b0;
                 clear1set <= 1'b0;
                 clear2set <= 1'b0;
                 clear3set <= 1'b0;
@@ -50,16 +62,18 @@ always@(posedge clk)
 				yIncremented = 1'b0;
 				movedDown <= 1'b0;
 				aliensMoveEn <= 1'b0;
+				colourOut <= 3'b0;
 			end
 		if (~startRow) begin //draw initial row of aliens
-			if (yout < yStart + 7'd10) //CHANGE TO ACCOUNT FOR 114 EXTRA CYCLE IN yout?
+			
+			if (yout < (yStart + 7'd10)) //CHANGE TO ACCOUNT FOR 114 EXTRA CYCLE IN yout?
 				begin
 					if (xout == 8'd159)
 						begin
 							xout <= 8'd0; //reset to original position
 							yout <= yout + 1;
-							colourOut <= aliensColour;
 							address <= address + 1;	
+							colourOut <= aliensColour;
 							aliensMoveEn <= 1'b1;
 						end
 					else
@@ -73,206 +87,284 @@ always@(posedge clk)
 				
 			else
 				begin
-					startRow <= 1'b1;
-					address <= 7'b0;
-					aliensMoveEn <= 1'b1;
+					startRow <= 1'b1; //mbe come back
+					address <= 11'b0;
+					aliensMoveEn <= 1'b0;
+					yout <= yorig;
 				end
 		end 
 
 		if (clear1) //clear high stays high until done - use assign statements outside always block to originally set 
-            if (!clear1set)
+            begin
+				if (!clear1set)
 				begin
-					yout <= 7'd10;
-					xout <= 7'd10;
-					clear1set = 1'b1;
+					yout <= ALIEN1_Y; 
+					xout <= ALIEN1_X;
+					address <= 11'd10; //check this right start position
+					clear1set <= 1'b1;
+					aliensMoveEn <= 1'b1;
+					clearAny <= 1'b1;
+					
 				end  
-            begin 
-                if (yout < 7'd21)  //clear
-				begin
-					if (xout == 8'd22)
+
+			
+             if (yout < (ALIEN1_Y + 7'd10))  //this will usually take in y coord from FSM - test 1st for testing
 						begin
-							xout <= 8'd0; //reset to original
-							yout <= yout + 1;
-							colourOut <= 3'b0;
+							if (xout == ALIEN1_X + 8'd11)
+								begin
+									xout <= ALIEN1_X; //reset to original
+										if (yout < (yorig + 7'd10)) begin yout <= yout + 1; end
+										
+									colourOut <= 3'b0;
+									clearAny <= 1'b1;
+									address <= address + 11'd149; // change
+								end
+							else
+								begin
+									xout <= xout + 1;
+									colourOut <= 3'b0;
+									clearAny <= 1'b1;
+									address <= address + 1;
+								end
 						end
 					else
 						begin
-							xout <= xout + 1;
-							colourOut <= 3'b0;
-						end
-				end
-            
-                //write to memory
-				if (address < 11'd220) //TODO / CHECK
-					address <= address + 1;
-				else
-					begin
-						address <= 11'b0;
 						cleared1 <= 1'b1;
 						clearAny <= 1'b0;
-					end
-
+						aliensMoveEn <= 1'b0;
+						end
+		
 			end
+
+//TODO
+				// begin
+				// if (address < 11'd540)
+				// 	address <= address + 1;
+				// else
+				// 	begin
+				// 		address <= 11'b0;
+				// 		cleared2 <= 1'b1;
+				// 		clearAny <= 1'b0;
+				// 		aliensMoveEn <= 1'b0;
+				// 	end
+				// end
+
+				
+		
 		else if (clear2) //clear high stays high until done 
+		begin
             if (!clear2set)
 				begin
 					yout <= 7'd10;
-					xout <= 7'd43;
-					clear2set = 1'b1;
+					xout <= 7'd42;
+					clear2set <= 1'b1;
+					clearAny <= 1'b1;
+					aliensMoveEn <= 1'b1;
+					address <= 11'h2a; //FIRST ADDRESS
 				end
-			begin 
-                if (yout < 7'd21)  //clear
-				begin
-					if (xout == 8'd54)
+
+            if (yout < (ALIEN1_Y + 7'd10))  //clear
+					begin
+					if (xout == 8'd53)
 						begin
-							xout <= 8'd0; //reset to original
+							xout <= 8'd42; //reset to original
 							yout <= yout + 1;
 							colourOut <= 3'b0;
+							address <= address + 11'd149;
 						end
 					else
 						begin
 							xout <= xout + 1;
 							colourOut <= 3'b0;
+							clearAny <= 1'b1;
+							address <= address + 1;
 						end
-				end
-
-
-				if (address < 11'd540)
-					address <= address + 1
-				else
-					begin
-						address <= 11'b0;
-						cleared2 <= 1'b1;
-						clearAny <= 1'b0;
 					end
+
+				// begin
+				// if (address < 11'd540)
+				// 	address <= address + 1;
+				// else
+				// 	begin
+				// 		address <= 11'b0;
+				// 		cleared2 <= 1'b1;
+				// 		clearAny <= 1'b0;
+				// 		aliensMoveEn <= 1'b0;
+				// 	end
+				// end
+				
 			end
 		else if (clear3) //clear high stays high until done 
+		begin
 			if (!clear3set)
 				begin
 					yout <= 7'd10;
-					xout <= 7'd75;
-					clear3set = 1'b1;
+					xout <= 7'd74;
+					clear3set <= 1'b1;
+					aliensMoveEn <= 1'b1;
+					address <= 11'h4a;
 				end
+				
             begin 
-                if (yout < 7'd21)  //clear
+            if (yout < (ALIEN1_Y + 7'd10))  //clear
 				begin
-					if (xout == 8'd86)
+					if (xout == 8'd85)
 						begin
-							xout <= 8'd0; //reset to original
+							xout <= 8'd74; //reset to original
 							yout <= yout + 1;
 							colourOut <= 3'b0;
+							address <= address + 11'd149;
 						end
 					else
 						begin
 							xout <= xout + 1;
 							colourOut <= 3'b0;
+							clearAny <= 1'b1;
+							address <= address + 1;
 						end
 				end
 
-
-				if (address < 11'd860)
-					address <= address + 1
-				else
-					begin
-						address <= 11'b0;
-						cleared3 <= 1'b1;
-						clearAny <= 1'b0;
-					end
+			
+				// begin
+				// if (address < 11'd860)
+				// 	address <= address + 1;
+				// else
+				// 	begin
+				// 		address <= 11'b0;
+				// 		cleared3 <= 1'b1;
+				// 		clearAny <= 1'b0;
+				// 		aliensMoveEn <= 1'b0;
+				// 	end
+				// end	
+					
+			end
 			end
 		else if (clear4) //clear high stays high until done 
+			begin
 			if (!clear4set)
 				begin
 					yout <= 7'd10;
-					xout <= 7'd107;
-					clear4set = 1'b1;
+					xout <= 7'd106;
+					clear4set <= 1'b1;
+					address <= 11'h6a;
+					aliensMoveEn <= 1'b1;
 				end
             
             begin 
 
-                if (yout < 7'd21)  //clear
+                if (yout < (ALIEN1_Y + 7'd10))  //clear
 				begin
-					if (xout == 8'd118)
+					if (xout == 8'd117)
 						begin
-							xout <= 8'd0; //reset to original
+							xout <= 8'd106; //reset to original
 							yout <= yout + 1;
 							colourOut <= 3'b0;
+							address <= address + 11'd149;
 						end
 					else
 						begin
 							xout <= xout + 1;
 							colourOut <= 3'b0;
+							clearAny <= 1'b1;
+							address <= address + 1;
+							// address <= 11'd1059;
 						end
 				end
 
 
 
-
-				if (address <= 11'd1180)
-					address <= address + 1
-				else
-					begin
-						address <= 11'b0;
-						cleared4 <= 1'b1;
-						clearAny <= 1'b0
-					end
+				// begin
+				// if (address <= 11'd1180)
+				// 	address <= address + 1;
+				// else
+				// 	begin
+				// 		address <= 11'b0;
+				// 		cleared4 <= 1'b1;
+				// 		clearAny <= 1'b0;
+				// 		aliensMoveEn <= 1'b0;
+				// 	end
+				// end
+				
+			end
 			end
 		else if (clear5) //clear high stays high until done 
+		begin
 			if (!clear5set)
 				begin
-					yout <= 7'd10;
-					xout <= 7'd139;
-					clear5set = 1'b1;
+					yout <= 7'd10; //reset these as well
+					xout <= 7'd138;
+					clear5set <= 1'b1;
+					aliensMoveEn <= 1'b1;
+					address <= 1'h8a;
 				end
             
             
             begin 
 
-                if (yout < 7'd150)  //clear
+                if (yout < (ALIEN1_Y + 7'd10))  //clear
 				begin
-					if (xout == 8'd86)
+					if (xout == 8'd149)
 						begin
-							xout <= 8'd0; //reset to original
+							xout <= 8'd138; //reset to original
 							yout <= yout + 1;
 							colourOut <= 3'b0;
+							address <= address + 11'd149;
 						end
 					else
 						begin
 							xout <= xout + 1;
 							colourOut <= 3'b0;
+							clearAny <= 1'b1;
+							// address <= 11'd1379;
+							address <= address + 1;
 						end
 				end
 
-				if (address < 11'd1500)
-					address <= address + 1
-				else
-					begin
-						address <= 11'b0;
-						cleared5 <= 1'b1;
-						clearAny <= 1'b0;
-					end
+				// begin
+				// if (address < 11'd1500)
+				// 	address <= address + 1;
+				// else
+				// 	begin
+				// 		address <= 11'b0;
+				// 		cleared5 <= 1'b1;
+				// 		clearAny <= 1'b0;
+				// 		aliensMoveEn <= 1'b0;
+				// 	end
+				// end
+					
 			end
-		else address <= address; //do nothing
+			end
+	
 
+		begin
 		if (moveDown && ~aliensCleared) //clear original
-			if (yout < yorig + 7'd10) 
+		begin
+			if (yout < (yorig + 7'd10)) 
 				begin
 					if (xout == 8'd159)
 						begin
 							xout <= 8'd0; //reset to original
 							yout <= yout + 1;
 							colourOut <= 3'b0;
+							aliensMoveEn <= 1'b1;
 						end
 					else
 						begin
 							xout <= xout + 1;
 							colourOut <= 3'b0;
+							aliensMoveEn <= 1'b1;
 						end
 				end
+			
 			else 
 				begin
-					aliensCleared <= 1'b1;
+					aliensCleared = 1'b1;
 					yout <= yorig;
+					aliensMoveEn <= 1'b0;
 				end
+			end
+		end 
+		
 		if (aliensCleared && moveDown) begin //draw row shift down - increment y by 5 then read memory addresses again
 			if (!yIncremented)
 				begin
@@ -280,14 +372,15 @@ always@(posedge clk)
 					yout <= yorig + 5;
 					xout <= 8'd0;
 					yIncremented = 1'b1;
+					aliensMoveEn <= 1'b1;
 				end
 				
 			//row cleared - draw alien row below 
-			else if (yout < yorig + 7'd11) //CHANGE TO ACCOUNT FOR 114 EXTRA CYCLE IN yout?
+			else if (yout < yorig + 7'd10) //CHANGE TO ACCOUNT FOR 114 EXTRA CYCLE IN yout?
 				begin
 					if (xout == 8'd159)
 						begin
-							xout <= xorig; //reset to original position
+							xout <= 8'd0; //reset to original position
 							yout <= yout + 1;
 							colourOut <= aliensColour;
 							address <= address + 1;	
@@ -305,19 +398,22 @@ always@(posedge clk)
 					movedDown <= 1'b1;
 					address <= 11'b0;
 					aliensMoveEn <= 1'b0;
+					aliensCleared = 1'b0; //this necessary
+					yIncremented = 1'b0;
 				end
 		end
-	end
 
 
-	if (gameOver)
+
+	if (gameDone)
 		begin 
 			if (xout == X_SCREEN_PIXELS - 1 && yout == Y_SCREEN_PIXELS - 1) 
 				begin
 					xout <= 8'd0; 
-					yout <= yorig;
+					yout <= 7'd0;
 					gameDone <= 1'b1;
 					addressFS <= 15'b0;
+					aliensMoveEn <= 1'b0;
 				end
 			else
 				begin
@@ -326,7 +422,8 @@ always@(posedge clk)
 							xout <= 8'b0;
 							yout <= yout + 1;
 							addressFS <= addressFS + 1;
-                            colourOut <= gameOverColour;
+                     colourOut <= gameOverColour;
+							aliensMoveEn <= 1'b1;
 						end
 					else
 						begin
@@ -337,14 +434,15 @@ always@(posedge clk)
 				end
 		end
 
-	if (youWin)
+	if (gameWon)
 		begin 
 			if (xout == X_SCREEN_PIXELS - 1 && yout == Y_SCREEN_PIXELS - 1) 
 				begin
-					xout <= xorig; 
-					yout <= yorig;
+					xout <= 8'd0; 
+					yout <= 7'd0;
 					gameWon <= 1'b1;
 					addressFS <= 15'b0;
+					aliensMoveEn <= 1'b0;	
 				end
 			else
 				begin
@@ -353,7 +451,8 @@ always@(posedge clk)
 							xout <= 8'b0;
 							yout <= yout + 1;
 							addressFS <= addressFS + 1;
-                            colourOut <= youWinColour;
+                     colourOut <= youWinColour;
+							aliensMoveEn <= 1'b1;
 						end
 					else
 						begin
@@ -363,7 +462,7 @@ always@(posedge clk)
 						end
 				end
 		end
-
+end
 
 //reset clearAny to 0 once completed , set cleared to 1
 
