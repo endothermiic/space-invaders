5ce5ac905e3ebd98bc1233851ab6f4acfea15d81
endothermiic <103846051+endothermiic@users.noreply.github.com>
2023-12-05 11:21:16 -0500

    update with aliens

--- rocketMove/spaceInvaders.v
+++ rocketMove/spaceInvaders.v
@@ -1,6 +1,7 @@
 //`include "vga_controller.v"
 //`include "vga_address_translator.v"
 //`include "vga_pll.v"
+//`include "vga_adapter.v"
 
 module spaceInvaders
 	(
@@ -8,6 +9,7 @@ module spaceInvaders
 		// Your inputs and outputs here
 		HEX0, 
 		HEX2,
+		HEX4,
 		KEY,
 		SW,
 		LEDR,
@@ -26,6 +28,7 @@ module spaceInvaders
 	input			CLOCK_50;				//	50 MHz
 	output [7:0]HEX0;
 	output [7:0]HEX2;
+	output [7:0]HEX4;
 	output [9:0] LEDR;
 	input [3:0]KEY;
 	input [6:0]SW;
@@ -53,7 +56,8 @@ module spaceInvaders
 	wire [6:0] alientopY, alienBottomY;
 	wire [2:0] scoreCount;
 	
-	wire reset, start, left, right, shot; 
+	wire reset, start, left, right, shot, collidedWithAlien; 
+	wire [4:0] curState;
 	assign reset = ~SW[0];
 	assign start = ~KEY[3];
 	assign left = ~KEY[2];
@@ -62,46 +66,49 @@ module spaceInvaders
 	assign LEDR[1] = start;
 	
    rocket r0 (.reset(reset), .clk(clk), .start(start), .left(left), .right(right), 
-   .screenCleared(LEDR[0]), .drewHomeBase(LEDR[9]), .xout(r_xout), .yout(r_yout), .colourOut(r_colour), .drawEn(r_drawEn), .screenClearEn(LEDR[2]));
-  
-      
-   shots s0 (.clk(clk), .keyPressed(shot), .reset(reset), .xin(r_xout), .bulletX(b_xout), .bulletY(b_yout), .colour(b_colour), .drawEn(b_drawEn));
+   .screenCleared(LEDR[0]), .drewHomeBase(LEDR[9]), .xout(r_xout), .yout(r_yout), .colourOut(r_colour), .drawEn(r_drawEn), .curState(curState));
+
+	hex_decoder c9 (.c(r_drawEn), .display(HEX0)); 
+	hex_decoder c10 (.c(r_xout), .display(HEX2)); 
+	hex_decoder c11 (.c(curState), .display(HEX4)); //for debugging!
 	
-	aliensDatapath a1(.clk(clk), .reset(reset), .xtop(alienTopX), .ytop(alienTopY), .xbottom(alienBottomX), .ybottom(alienBottomY), .clear1(clear1), .clear2(clear2), .clear3(clear3), .clear4(clear4), .clear5(clear5), 
-				.moveDown(movedDown), .xout(a_xout), .yout(a_yout), .colourOut(a_colour), .aliensMoveEn(a_drawEn), .cleared1(cleared1), .cleared2(cleared2), .cleared3(cleared3), .cleared4(cleared4), .cleared5(cleared5),
-				.movedDown(movedDown), .clearAny(collidedWithAlien));
-				
-				
-	alienTalentManager a2 (.clk(clk), .reset(reset), .shotXcoord(b_xout), .shotYcoord(b_yout), .gameOver(gameOver),
-									.youWin(youWin), .scoreCount(scoreCount), 
-									.kill1(clear1), .kill2(clear2), .kill3(clear3), .kill4(clear4), .kill5(clear5), .moveDown(moveDown),
-									.clearedShift(movedDown), .cleared1(cleared1), .cleared2(cleared2), .cleared3(cleared3), .cleared4(cleared4), .cleared5(cleared5),
-									.alienTopY(alienTopY), .alienTopX(alienTopX), .alienBottomX(alienBottomX), .alienBottomY(alienBottomY));
-				
-			
+	      
+  shots s0 (.clk(clk), .keyPressed(shot), .reset(reset), .xin(r_xout), .bulletX(b_xout), .bulletY(b_yout), .colour(b_colour), .drawEn(b_drawEn), .collidedWithAlien(collidedWithAlien));
 	
-	
-	//aliensMove g0 (.clk(clk), .reset(reset), .xin, .yin, .clear1, .clear2, .clear3, .clear4, 
-				//	.clear5, .moveDown, .xout(a_xout), .yout(a_yout), .colourOut(a_colour),  
-			//.cleared1, .cleared2, .cleared3, .cleared4, .cleared5, .movedDown
-	
-	
-	//TO DO
-	//instantiate controlPath from sofiia as well 
-	//add DRAW ENABLE into CONTROL PATH
-	//make big fsm for GAME ! - ensure bullet/aliens/rocket control signals go to vga
+  aliens (.clk(clk), .reset(reset), .xout(a_xout), .yout(a_yout), .colourOut(a_colour), .a_drawEn(a_drawEn), .shotXCoord(b_xout), .shotYCoord(b_yout), .score(scoreCount), .collidedWithAlien(collidedWithAlien));
 	
 		
 	// Create an Instance of a VGA controller - there can be only one!
 	// Define the number of colours as well as the initial background
 	// image file (.MIF) for the controller.
 	
+//		vga_adapter VGA(
+//			.resetn(~SW[0]),
+//			.clock(CLOCK_50),
+//			.colour(r_colour),
+//			.x(r_xout),
+//			.y(r_yout),
+//			.plot(r_drawEn),
+//			/* Signals for the DAC to drive the monitor. */
+//			.VGA_R(VGA_R),
+//			.VGA_G(VGA_G),
+//			.VGA_B(VGA_B),
+//			.VGA_HS(VGA_HS),
+//			.VGA_VS(VGA_VS),
+//			.VGA_BLANK(VGA_BLANK_N),
+//			.VGA_SYNC(VGA_SYNC_N),
+//			.VGA_CLK(VGA_CLK));
+//		defparam VGA.RESOLUTION = "160x120";
+//		defparam VGA.MONOCHROME = "FALSE";
+//		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
+//		defparam VGA.BACKGROUND_IMAGE = "newHome.mif";
+//	
 	vga_adapter VGA(
 			.resetn(~SW[0]),
 			.clock(CLOCK_50),
 			.colour(a_drawEn ? a_colour : r_drawEn ? r_colour : b_drawEn ? b_colour :0),
 			.x(a_drawEn ? a_xout : r_drawEn ? r_xout : b_drawEn ? b_xout :0),
-			.y(a_drawEn ? a_yout : r_drawEn ? r_you : b_drawEn ? b_yout :0),
+			.y(a_drawEn ? a_yout : r_drawEn ? r_yout : b_drawEn ? b_yout :0),
 			.plot(a_drawEn | r_drawEn | b_drawEn),
 			/* Signals for the DAC to drive the monitor. */
 			.VGA_R(VGA_R),
@@ -116,9 +123,8 @@ module spaceInvaders
 		defparam VGA.MONOCHROME = "FALSE";
 		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
 		defparam VGA.BACKGROUND_IMAGE = "newHome.mif";
-			
+////			
 	// Put your code here. Your code should produce signals x,y,colour and writeEn
 	// for the VGA controller, in addition to any other functionality your design may require.
-	hexdecoder u7 (.c(scoreCount), .display(HEX[0]));
 
 endmodule
